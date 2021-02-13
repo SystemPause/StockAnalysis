28b236889eb7d26e09617c7ef8db0fc6b344d4f8
Alessandro Speggiorin <alessandrospeggiorin@gmail.com>
2021-02-13 18:17:21 +0100

    Added disconnect vpn

--- mainScriptNordPVN.sh
+++ mainScriptNordPVN.sh
@@ -5,12 +5,16 @@ declare -a screenerUrls=("day_gainers" "day_losers" "undervalued_large_caps" "mo
 # Get all the stock info and save them as files. 
 for url in "${screenerUrls[@]}"
 do
+    # Connect to vpn 
     sudo nordvpn connect 
     sudo nordvpn status
     echo $url
     python3 Main.py auto "https://uk.finance.yahoo.com/screener/predefined/${url}"
     
 done
+
+# Once done disconnect from the vpn to prevent problems with the vpn + email
+sudo nordvpn disconnect
 
 # Send the email containing the files
 # But first merge them all 
